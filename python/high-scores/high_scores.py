b82d38bcbbf2f34e659af98e59a7a53dde28ac5c
Beatrice Wittgren <bea@klira.io>
2020-02-27 16:48:13 +0100

    improved solution inspired by others. tests succesful

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -7,17 +7,24 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
-    top_three = []
+    return sorted(scores, reverse=True)[:3]
 
-    # sets number of iterations
-    it = 3
-    if len(scores) < 3:
-        it = len(scores)
-
-    # collects top three scores
-    for _ in range(it):
-        best = max(scores)
-        top_three.append(best)
-        scores.remove(best)
-
-    return top_three
+# I N T E R E S T I N G   S O L U T I O N   B Y   O T H E R
+#    import heapq
+#    top3 = heapq.nlargest(3,scores)
+#    return top3
+# M Y   F I R S T   S O L U T I O N
+#    top_three = []
+#
+#    # sets number of iterations
+#    it = 3
+#    if len(scores) < 3:
+#        it = len(scores)
+#
+#    # collects top three scores
+#    for _ in range(it):
+#        best = max(scores)
+#        top_three.append(best)
+#        scores.remove(best)
+#
+#    return top_three
