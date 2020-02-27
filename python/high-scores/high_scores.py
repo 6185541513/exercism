8b326ec2c17e314ed601b777c7527929315a419b
Beatrice Wittgren <bea@klira.io>
2020-02-27 16:33:55 +0100

    adds implemtentation. tests succesful

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,23 @@
 def latest(scores):
-    pass
+    return scores.pop()
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    top_three = []
+
+    # sets number of iterations
+    it = 3
+    if len(scores) < 3:
+        it = len(scores)
+
+    # collects top three scores
+    for _ in range(it):
+        best = max(scores)
+        top_three.append(best)
+        scores.remove(best)
+
+    return top_three
