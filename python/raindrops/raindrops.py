16928e6380585a34ec04f09476be5144b686db15
Beatrice Wittgren <bea@klira.io>
2020-02-27 12:58:33 +0100

    implemnt raindrop task. tests succesfull

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,17 @@
+import math
+
+
 def convert(number):
-    pass
+    result = ""
+
+    if math.isnan(number):
+        raise Exception("'number' is NaN")
+
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+
+    return result or """{}""".format(number)
