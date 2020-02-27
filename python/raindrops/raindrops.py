a40d54faebf963aae2a4cfea74c8a5baad524be5
Beatrice Wittgren <bea@klira.io>
2020-02-27 15:25:53 +0100

    updates solution after inspiration other ppl. test succesful

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,16 +2,31 @@ import math
 
 
 def convert(number):
-    result = ""
+    FACTOR_SOUNDS = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
+    sound = ''
 
+    # exclude NaN
     if math.isnan(number):
         raise Exception("'number' is NaN")
 
-    if number % 3 == 0:
-        result += "Pling"
-    if number % 5 == 0:
-        result += "Plang"
-    if number % 7 == 0:
-        result += "Plong"
+    # build sound
+    for factor in FACTOR_SOUNDS.keys():
+        if number % factor == 0:
+            sound += FACTOR_SOUNDS.get(factor)
 
-    return result or """{}""".format(number)
+    return sound or str(number)
+
+# F I R S T   S O L U T I O N
+#    result = ""
+#
+#    if math.isnan(number):
+#        raise Exception("'number' is NaN")
+#
+#    if number % 3 == 0:
+#        result += "Pling"
+#    if number % 5 == 0:
+#        result += "Plang"
+#    if number % 7 == 0:
+#        result += "Plong"
+#
+#    return result or """{}""".format(number)
