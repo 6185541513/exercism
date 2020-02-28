aa2740dd7a1586d885f553f793690494a31d3925
Beatrice Wittgren <bea@klira.io>
2020-02-28 16:18:56 +0100

    not pretty work in process..

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,29 @@
+import math
+
+
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        # create rows
+        rows_string = [[int(elem) for elem in row] for row in [rows_unformatted.split() for rows_unformatted in matrix_string.splitlines()]]
+
+        for elem in rows_string:
+            print("!!!!!!!!!!!"+str(elem))
+
+        # create columns
+        number_of_columns = len(rows_string[0])
+        columns_string = []
+        for i in range(number_of_columns):
+            column = []
+            for row in rows_string:
+                column.append(row[i])
+            columns_string.append(column)
+
+        # string -> int
+        self.rows = list(map(int, rows_string))
+        self.columns = list(map(int, columns_string))
 
     def row(self, index):
-        pass
+        return self.rows[index]
 
     def column(self, index):
-        pass
+        return self.columns[index]
