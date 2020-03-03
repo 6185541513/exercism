0714bde4d5d4cc66b52e2442eee73af5f408532e
Beatrice Wittgren <bea@klira.io>
2020-03-03 09:46:24 +0100

    updates two_fer after mentor advise. tests suceesful

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,7 @@
 def two_fer(name="you"):
-    return """One for {}, one for me.""".format(name)
+    return f'One for {name}, one for me.'
+
+
+# F I R S T   S O L U T I O N
+# def two_fer(name="you"):
+#    return """One for {}, one for me.""".format(name)
