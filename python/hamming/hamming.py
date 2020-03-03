e103cd508a1cf36d1dbe6974be63e9f0f3423929
Beatrice Wittgren <bea@klira.io>
2020-03-03 11:50:27 +0100

    improved hamming solution with zip(). tests succesful

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,8 +2,11 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The strands differ in length")
 
-    count = 0
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            count = count + 1
-    return count
+    return sum(a != b for a, b in zip(strand_a, strand_b))
+
+# F I R S T   S O L U T I O N
+#    count = 0
+#    for i in range(len(strand_a)):
+#        if strand_a[i] != strand_b[i]:
+#            count = count + 1
+#    return count
