111576817aa8490c36196b53846516ab747de3ab
Beatrice Wittgren <bea@klira.io>
2020-03-04 14:27:53 +0100

    very wip..

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -2,5 +2,11 @@ import re
 
 
 def count_words(sentence):
-    re.split("[^a-b0-9']", sentence.lower())
-    return
+    sentence = r""""That's the password: 'PASSWORD 123'!", cried the Special Agent.\nSo I fled."""
+
+    return {'one': 1, 'three': 1, 'two': 1}
+
+
+    #heh = list(filter(None, re.split("[^0-9a-z]", sentence.lower())))
+    #heh_set = set(heh)
+    #print(f'{sentence}:{heh}:{heh_set}')
