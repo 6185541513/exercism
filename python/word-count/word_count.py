8577279e0ee92b808f6180f82fddf02dd40de810
Beatrice Wittgren <bea@klira.io>
2020-03-05 10:17:29 +0100

    implemented solution inspired by others. tests successful

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,12 +1,9 @@
 import re
+from collections import Counter
 
 
 def count_words(sentence):
-    sentence = r""""That's the password: 'PASSWORD 123'!", cried the Special Agent.\nSo I fled."""
+    return Counter(re.findall(r"[a-z0-9]+'?[a-z]*(?<!')", sentence.casefold()))
 
-    return {'one': 1, 'three': 1, 'two': 1}
-
-
-    #heh = list(filter(None, re.split("[^0-9a-z]", sentence.lower())))
-    #heh_set = set(heh)
-    #print(f'{sentence}:{heh}:{heh_set}')
+# F I R S T   S O L U T I O N
+# (╯°□°）╯︵ ┻━┻
