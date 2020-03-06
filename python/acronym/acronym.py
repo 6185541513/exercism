2462bb0219e52423b71526c5e826db9d0d90c4fa
Beatrice Wittgren <bea@klira.io>
2020-03-06 17:14:47 +0100

    updated acronym inspired by others. tests succesful

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,4 +2,7 @@ import re
 
 
 def abbreviate(words):
-    return "".join(list(map(lambda word: str(word[0].upper()), re.findall("[A-Z]+'?[A-Z]*", words.upper()))))
+    return "".join(re.findall(r"([A-Z])[A-Z']*", words.upper()))
+
+# F I R S T   S O L U T I O N
+# return "".join(list(map(lambda word: str(word[0]), re.findall("[A-Z]+'?[A-Z]*", words.upper()))))
