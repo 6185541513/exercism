e92189159577a5d07a96364dd9764da5d458efc7
Beatrice Wittgren <bea@klira.io>
2020-03-18 14:57:32 +0100

    wip improved solution

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -8,16 +8,24 @@ class Garden:
         self.students.sort()
 
     def plants(self, student):
+        student_index = self.students.index(student)
+        print(f'{student}:{student_index}')
+        plant_letters = ''.join(map(lambda plant_row: plant_row[student_index*2:student_index*2+2], self.diagram))
+        print(f'{self.diagram}')
+        print(f'{plant_letters}:{student}')
+        student_plants = [(plant_name for plant_name in plants if letter == plant_name[0]) for letter in plant_letters]
+        print(f'{student_plants}')
+        next()
+
+
         plant_letters = []
         for i in range(len(self.students)):
             if student == self.students[i]:
-                print(f'{student} {i}:{i+1}')
                 for pll in self.diagram:
                     plant_letters.append(pll[i*2])
                     plant_letters.append(pll[i*2+1])
 
         result = []
-        print(str(plant_letters))
         for pl in plant_letters:
             for p in plants:
                 if pl == p[0]:
@@ -30,4 +38,4 @@ Eve, Fred, Ginny, Harriet,
 Ileana, Joseph, Kincaid, and Larry.
         """)))  # took longer and is ugly but at least... hmm
 
-plants = list(filter(None, re.split(r',|\s|And', """grass, clover, radishes, and violets""".title())))  # why
+plants = set(filter(None, re.split(r',|\s|And', """grass, clover, radishes, and violets""".title())))  # why
